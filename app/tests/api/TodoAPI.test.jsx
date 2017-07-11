4b0b9ed98e200e576a7e8239dca186387bd94eb8
Jacob <jacobmoyle@gmail.com>
2017-07-10 18:14:29 -0700

    expand filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -82,5 +82,22 @@ describe('TodoAPI', () => {
       expect(filteredTodos.length).toBe(1);
     });
 
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all todos if searchText is empty', () => {
+
+    });
+
   });
 });
