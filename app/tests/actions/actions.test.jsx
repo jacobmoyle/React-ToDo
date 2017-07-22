f1806b7ae471cee6f0ad077f2c22906ccdf5882d
Jacob <jacobmoyle@gmail.com>
2017-07-22 12:35:48 -0700

    create action generators

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,44 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', () => {
+
+  it('should generate search text action', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'Some search text'
+      };
+      var result = actions.setSearchText(action.searchText);
+
+      expect(result).toEqual(action);
+  });
+
+  it('should generate addTodo action', () => {
+    var action = {
+      type: 'ADD_TODO',
+      text: 'Some text'
+    };
+    var result = actions.addTodo(action.text);
+
+    expect(result).toEqual(action);
+  });
+
+  it('should generate toggleShowCompleted action', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED',
+    };
+    var result = actions.toggleShowCompleted();
+
+    expect(result).toEqual(action);
+  });
+
+  it('should generate toggleTodo action', () => {
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: 9001
+    };
+    var result = actions.toggleTodo(action.id);
+
+    expect(result).toEqual(action);
+  });
+});
