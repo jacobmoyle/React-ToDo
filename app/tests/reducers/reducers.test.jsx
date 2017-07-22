5301ef5f1247fcf46cd7b8e8bbc64023e63bcccd
Jacob <jacobmoyle@gmail.com>
2017-07-22 13:04:30 -0700

    create search and completed reducers

---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,29 @@
+var expect = require('expect');
+var df = require('deep-freeze-strict');
+
+var reducers = require('reducers');
+
+describe('reducers', () => {
+  describe('searchTextReducer', () => {
+    it('should set search text', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'dog'
+      };
+      var response = reducers.searchTextReducer(df(''), df(action));
+
+      expect(response).toEqual(action.searchText);
+    });
+  });
+
+  describe('showCompletedReducer', () => {
+    it('should update showCompleted', () => {
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      };
+      var response = reducers.showCompletedReducer(df(false), df(action));
+
+      expect(response).toBe(true);
+    });
+  });
+});
